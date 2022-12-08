1a6b53862f892d8045c5ffa4983809427b866191
mathiaskliem <kliem.mathias@gmail.com>
2022-12-08 09:52:58 +0100

    continuer page contact

--- vue/vueAccueil.php
+++ vue/vueAccueil.php
@@ -2,7 +2,7 @@
 $title = "Château Bourbon";
 ob_start();
 ?>
-    <video playsinline autoplay muted loop id="bgvid">
+    <video playsinline="true" autoplay="true" muted="true" loop="true" id="bgvid">
         <source src="video/Chateau_mobile_1080p_Medium.mp4" type="video/mp4">
     </video>
     <div class="centerIndex">
