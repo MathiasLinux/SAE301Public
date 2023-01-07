66d681ee17897000a1ae7c84e0d5ea13d7fcce14
mathiaskliem <kliem.mathias@gmail.com>
2023-01-07 11:36:25 +0100

    changement du filtre de recherche de chateaux

--- js/chateau.js
+++ js/chateau.js
@@ -13,6 +13,7 @@ console.log(input);
 
 let biens = document.querySelectorAll(".contourBiens");
 
+/*
 input.forEach(e => {
     e.addEventListener("change", () => {
         //If the input changed, we call the function
@@ -23,4 +24,48 @@ input.forEach(e => {
             }
         })
     });
-})
+})*/
+
+/*
+document.querySelectorAll(".selecteurCase .labelEtInput input").forEach(e => {
+    e.addEventListener("click", () => {
+        biens.forEach(bien => {
+            if (!(bien.getAttribute("data-epoque") == e.name && e.checked == true)) {
+                bien.classList.add("bienHidden");
+            } else {
+                bien.classList.remove("bienHidden");
+                console.log(e);
+            }
+        });
+    });
+});*/
+
+function filtreEpoque(epoque) {
+    console.log(epoque);
+    selector = "input[name=" + epoque + "]";
+    document.querySelector(selector).addEventListener("click", () => {
+        biens.forEach(bien => {
+            if ((document.querySelector(selector).checked == true)) {
+                bien.classList.remove("bienHidden");
+            } else {
+                bien.classList.add("bienHidden");
+            }
+        });
+    });
+}
+
+input.forEach(e => {
+    e.addEventListener("change", () => {
+        filtreEpoque(e.name);
+        console.log(e.name);
+    });
+});
+
+/*
+filtreEpoque("XV");
+        filtreEpoque("XVI");
+        filtreEpoque("XVII");
+        filtreEpoque("XVIII");
+        filtreEpoque("XIX");
+        filtreEpoque("XX");
+ */
