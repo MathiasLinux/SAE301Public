ec6bd71d326aa52a3b35ca3bd66148ebf9044345
Mathias KLIEM <kliem.mathias@gmail.com>
2023-04-17 18:31:25 +0200

    security change

---
+++ config/config.php
@@ -0,0 +1,11 @@
+
+<?php
+// Connexion a la BDD
+// Adresse de l'hote
+define("DBHOST", "localhost");
+// Nom de la BDD
+define("DBNAME", "NAME");
+// Nom d'utilisateur
+define("DBUSER", "LOGIN");
+// Mot de passe
+define("DBPWD", "PASSWORD");
